8f51fe260099872df081dfb126c440023c3f5675
Dhall777 <48307756+Dhall777@users.noreply.github.com>
2020-01-16 19:22:04 -0800

    Delete records.hrl

--- records.hrl
+++
@@ -1,4 +0,0 @@
--record(sensor, {id, cx_id, name, vl, fanout_ids}).
--record(actuator, {id, cx_id, name, vl, fanin_ids}).
--record(neuron, {id, cx_id, af, input_idps, output_ids}).
--record(cortex, {id, sensor_ids, actuator, output_ids}).
